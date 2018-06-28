454c5948a4d0cc2e5b56e68f4a1a80d2e23fb06d
root <root@localhost.localdomain>
2018-06-28 11:06:10 +0530

    first commit

---
+++ src/main/java/com/Rishi/CurrencyConvert.java
@@ -0,0 +1,24 @@
+package com.Rishi;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import java.io.IOException;
+
+public class CurrencyConvert {
+    public static double convert(String c1,String c2) throws IOException {
+    HttpClient httpClient=new DefaultHttpClient();
+    HttpGet httpGet=new HttpGet("https://free.currencyconverterapi.com/api/v5/convert?q=" +c1.toUpperCase() +"_" +c2.toUpperCase() +"&compact=ultra");
+    HttpResponse httpResponse=httpClient.execute(httpGet);
+    HttpEntity entity=httpResponse.getEntity();
+    String responseString=EntityUtils.toString(entity,"UTF-8") ;
+    JSONObject jsonObject=new JSONObject(responseString);
+    Double value=jsonObject.getDouble(c1.toUpperCase() +"_" +c2.toUpperCase());
+    return value;
+
+}}
+
