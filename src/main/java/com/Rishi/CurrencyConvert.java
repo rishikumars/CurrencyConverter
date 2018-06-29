56a577b19f6670992f570846a344ac3695f892b7
rishikumars <rishikumar.siva@gmail.com>
2018-06-29 09:36:59 +0530

    update

--- src/main/java/com/Rishi/CurrencyConvert.java
+++ src/main/java/com/Rishi/CurrencyConvert.java
@@ -10,15 +10,27 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class CurrencyConvert {
-    public static double convert(String c1,String c2) throws IOException {
-    HttpClient httpClient=new DefaultHttpClient();
-    HttpGet httpGet=new HttpGet("https://free.currencyconverterapi.com/api/v5/convert?q=" +c1.toUpperCase() +"_" +c2.toUpperCase() +"&compact=ultra");
-    HttpResponse httpResponse=httpClient.execute(httpGet);
-    HttpEntity entity=httpResponse.getEntity();
-    String responseString=EntityUtils.toString(entity,"UTF-8") ;
-    JSONObject jsonObject=new JSONObject(responseString);
-    Double value=jsonObject.getDouble(c1.toUpperCase() +"_" +c2.toUpperCase());
-    return value;
+    public static double convert(String c1, String c2) throws IOException {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("https://free.currencyconverterapi.com/api/v5/convert?q=" + c1.toUpperCase() + "_" + c2.toUpperCase() + "&compact=ultra");
+        HttpResponse httpResponse = httpClient.execute(httpGet);
+        HttpEntity entity = httpResponse.getEntity();
+        String responseString = EntityUtils.toString(entity, "UTF-8");
+        JSONObject jsonObject = new JSONObject(responseString);
+        Double value = jsonObject.getDouble(c1.toUpperCase() + "_" + c2.toUpperCase());
+        return value;
+    }
 
-}}
+    public static double convert(int n, String c1, String c2) throws IOException
+    {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("https://free.currencyconverterapi.com/api/v5/convert?q=" + c1.toUpperCase() + "_" + c2.toUpperCase() + "&compact=ultra");
+        HttpResponse httpResponse = httpClient.execute(httpGet);
+        HttpEntity entity = httpResponse.getEntity();
+        String responseString = EntityUtils.toString(entity, "UTF-8");
+        JSONObject jsonObject = new JSONObject(responseString);
+        double value = jsonObject.getDouble(c1.toUpperCase() + "_" + c2.toUpperCase());
+        return value*n;
+
+    }}
 
